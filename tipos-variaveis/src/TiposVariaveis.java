52f2552013b51d14de0acb663d5c2b0d997d51aa
Luizsellitto <luizsellitto2609@gmail.com>
2025-05-03 12:54:28 -0300

    alguns tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,21 @@
 public class TiposVariaveis
 {
     public static void main(String[] args) {
-        
+        byte idade = 18;
+        short ano = 2025;
+        int cep = 1331040883;
+        long cpf = 33333333333L; //Terminar em L para saber que é tipo long
+        float pi = 3.14F; //Terminar em F para saber que é tipo float
+        double salario = 1234.56;
+
+        short numeroCurto = 1;
+        int numeroNormal = 2;
+        short numeroCurto2 = (short) numeroNormal; //Converte o tipo mais abrangente para um tipo mais específico
+
+
+        cep = 123;
+        final int CEP = 134; //final deixa a variável constante
+
+        String nome = "Luiz Augusto";
     }
 }
